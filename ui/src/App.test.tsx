edcb1363e71339ac1a0d774cbe4dac91f14e5594
sbkjarmul <sbkjarmul@gmail.com>
2023-05-23 07:06:08 +0200

    Change prettier settings to new programming style

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders text PizzaBlock', () => {
-    render(<App />);
-    const text = screen.getByText(/PizzaBlock/i);
-    expect(text).toBeInTheDocument();
-});
+  render(<App />)
+  const text = screen.getByText(/PizzaBlock/i)
+  expect(text).toBeInTheDocument()
+})
